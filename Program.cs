96240ab3b981cc2074fae816c668f49655d0922a
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-17 14:03:26 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_typesystem_snacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
