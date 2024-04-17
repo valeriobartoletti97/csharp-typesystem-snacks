3da14dc0e87bf04f85e90f5c60a3cecefaa3a9c5
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-17 14:36:40 +0200

    snack 2 finished

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@
             //Snack 1
             //L’utente inserisce due numeri in successione.
             //Il software stampa il maggiore.
-            Console.WriteLine("Inserisci un numero");
+
+            /*Console.WriteLine("Inserisci un numero");
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Inserisci un secondo numero");
             int num2 = Convert.ToInt32(Console.ReadLine());
@@ -24,6 +25,30 @@
             else
             {
                 Console.WriteLine("I numeri sono uguali");
+            } */
+
+            //Snack 2
+            //L’utente inserisce due parole in successione.
+            //Il software stampa prima la parola più corta, poi la parola più lunga.
+
+            Console.WriteLine("Inserisci una parola");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Inserisci una seconda parola");
+            string word2 = Console.ReadLine();
+            if (word1.Length != word2.Length)
+            {
+                if (word1.Length > word2.Length)
+                {
+                    Console.WriteLine($"La parola più lunga è {word1}");
+                }
+                else
+                {
+                    Console.WriteLine($"La parola più lunga è {word2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Le parole hanno la stessa lunghezza");
             }
         }
     }
