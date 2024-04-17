c6a1b52014af7943a2e6c67a316cb75767e6ad30
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-17 14:23:46 +0200

    snack 1 finished

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Snack 1
+            //L’utente inserisce due numeri in successione.
+            //Il software stampa il maggiore.
+            Console.WriteLine("Inserisci un numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci un secondo numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if(num1 != num2) 
+            {
+                if(num1 > num2)
+                {
+                    Console.WriteLine($"Il numero maggiore è {num1}");
+                } else
+                {
+                    Console.WriteLine($"Il numero maggiore è {num2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("I numeri sono uguali");
+            }
         }
     }
 }
